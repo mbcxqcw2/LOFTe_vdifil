67dca69bf1ea40616493914b6674b8096729728e
Walker <c.walker@cwalker-lt.local>
2022-11-25 15:52:16 +0000

    added header generation code

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
 # NOTES:
-CUDA code for filterbanking LOFT-e data.
-Original author: Mateusz Malenta
+Code for filterbanking LOFT-e data.
+
+Filterbanks are made using vdifil.cu.
+Requires header files which may be created using vdifil_headers.py. 
+
+Note: vdifil.cu only takes .vdif observations split into two separate polarisation files. If the two polarisations are stored in a single file, then these must be split up (using, e.g., baseband). 
+
+Authors: Mateusz Malenta (CUDA code), C. Walker (Python tools)
 
 # REQUIREMENTS:
 CUDA (more info to come)
 C++11
+LOFTe_parseVex: https://github.com/mbcxqcw2/LOFTe_parseVex (and dependencies)
 
-# TO MAKE:
+# TO MAKE THE FILTERBANKER:
 ```
-nvcc -o vdifil -std=c++11 vdifil.cu -lcufft
+>nvcc -o vdifil -std=c++11 vdifil.cu -lcufft
 ```
 
-# TO USE:
+# TO MAKE A FILTERBANK HEADER FILE:
+In python:
 ```
-./vdifil -a <data_pol0> -b <data_pol1> -o <outname.fil> -c <headerfile.dat> -s
+>make_vdifil_header(<vdif_file.vdif>,<vex_file.vex>,<header_file.dat>)
 ```
-where `<data_pol0>` is the polarisation 0 vdif file recorded for a LOFT-e observation, `<data_pol1>` is the polarisation 1 vdif file for the same observation, `<outname.fil>` is the desired output name for the filterbank file, and `-s` scales the output data to an 8-bit filterbank file. If `-s` is not included, the output filterbank file will be 32-bit.
+where `<vdif_file.vdif>' is an e-MERLIN .vdif file, `<vex_file.vex>` is its associated .vex file, and `<header_file.dat>` is the desired name of the output header file. 
+
+
+# TO USE THE FILTERBANKER:
+split the .vdif file into separate polarisatins `<pol0.vdif>` and `<pol1.vdif>` and then:
+```
+>./vdifil -a <pol0.vdif> -b <pol1.vdif> -o <outname.fil> -c <header_file.dat> -s
+```
+where `<outname.fil>` is the desired output name for the filterbank file, and `-s` scales the output data to an 8-bit filterbank file. If `-s` is not included, the output filterbank file will be 32-bit.
---
+++ vdifil_headers.py
@@ -0,0 +1,108 @@
+# Tools for creating a vdifil header for filterbanking vdif data
+# Author: C. Walker
+
+import os
+import numpy as np
+from LOFTe_parseVex_lib import parse_vex, get_vex_sched
+from baseband import vdif
+from matplotlib import pyplot as plt
+
+def make_vdifil_header(vdif_file,vex_file,outfilename):
+    """
+    This function generates a header file which can be used by LOFTe_vdifil
+    (https://github.com/mbcxqcw2/LOFTe_vdifil)
+    
+    to filterbank an e-MERLIN .vdif file
+    
+    INPUTS:
+    
+    vdif_file : [str] location of the .vdif file to filterbank
+    vex_file : [str] location of the .vdif file's associated .vex file
+    outfilename : [str] location and filename to store output header file
+    
+    RETURNS:
+    
+    a header file stored at outfilename for use with LOFTe_vdifil
+    
+    """
+    
+    ###############################
+    #hard-coded header information#
+    ###############################
+    #NOTE: these can be updated to generate automatically
+    #as more functionality is added to LOFTe_parseVex_lib
+
+    RAWFILE = 'LofteMkI'
+    AZ = 0.0
+    DEC = 0.0
+    RA = 0.0
+    REFDM = 0.0
+    TSAMP = 0.0
+    ZA = 0.0
+    DATATYPE=1
+    BEAMNO = 0
+    MACHINEID = 82
+    NOBEAMS = 0
+    OUTBITS = 32
+    NOCHANS = 256
+    NOIFS = 1
+    TELESCOPEID = 82
+    
+    ###############################
+    #read first frame of vdif data#
+    ###############################
+
+    fh = vdif.open(vdif_file,'rs')
+    TSTART = fh.start_time.mjd #extract start time of observation
+    fh.close()
+    
+    ####################
+    #parse the vex file#
+    ####################
+    
+    vex_data = parse_vex(vex_file) #extract all .vex file data
+    schedule_info = get_vex_sched(vex_data) #extract schedule information
+    
+    #extract information about the correct scan from vex file
+    scan_info = schedule_info[scan_number]
+    SOURCE=scan_info['scan_source']
+
+    #convert telescope names to all lowercase to match file naming scheme
+    alltels = np.array([i.lower() for i in scan_info['scan_stations']])
+
+    #get index for correct telescope
+    tel_idx = np.where(alltels==scan_tel)[0][0]
+
+    #get center frequency for observation in Hz
+    FCENT = scan_info['station_fcents'][tel_idx].to('Hz').value
+
+    #get bandwidth for observation in Hz
+    FOFF = scan_info['station_bandwidths'][tel_idx].to('Hz').value
+    
+    #write data to a header file
+    print('VDIF HEADER INFORMATION:\n')
+
+    with open(outfilename,"w") as f:
+        print('RAWFILE {0}'.format(RAWFILE),file=f)
+        print('SOURCE {0}'.format(SOURCE),file=f)
+        print('AZ {0}'.format(AZ),file=f)
+        print('DEC {0}'.format(DEC),file=f)
+        print('FCENT {0}'.format(FCENT),file=f)
+        print('FOFF {0}'.format(FOFF),file=f)
+        print('RA {0}'.format(RA),file=f)
+        print('REFDM {0}'.format(REFDM),file=f)
+        print('TSAMP {0}'.format(TSAMP),file=f)
+        print('TSTART {0}'.format(TSTART),file=f)
+        print('ZA {0}'.format(ZA),file=f)
+        print('DATATYPE {0}'.format(DATATYPE),file=f)
+        print('BEAMNO {0}'.format(BEAMNO),file=f)
+        print('MACHINEID {0}'.format(MACHINEID),file=f)
+        print('NOBEAMS {0}'.format(NOBEAMS),file=f)
+        print('OUTBITS {0}'.format(OUTBITS),file=f)
+        print('NOCHANS {0}'.format(NOCHANS),file=f)
+        print('NOIFS {0}'.format(NOIFS),file=f)
+        print('TELESCOPEID {0}'.format(TELESCOPEID),file=f)
+        
+    print('written to {0}'.format(outfilename))
+    
+    return
